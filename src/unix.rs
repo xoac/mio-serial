75fa8dcbb4fc5d3162041ebf3e454b1750c6f9b2
Sylwester Rąpała <sylwester.rapala@igrid.technology>
2018-10-06 18:43:15 +0200

    Use serialport v3.0.0

--- src/unix.rs
+++ src/unix.rs
@@ -9,12 +9,12 @@ use mio::{Evented, Poll, PollOpt, Ready, Token};
 use mio::unix::EventedFd;
 
 use serialport;
+use serialport::Result;
 use serialport::posix::TTYPort;
 use serialport::prelude::*;
 
-use nix::libc;
-use nix::sys::termios;
-use nix::sys::termios::{SetArg, SpecialCharacterIndices};
+use nix::{self, libc};
+use nix::sys::termios::{self, SetArg, SpecialCharacterIndices};
 
 /// *nix serial port using termios
 pub struct Serial {
@@ -133,8 +133,8 @@ impl SerialPort for Serial {
     }
 
     /// Return the name associated with the serial port, if known.
-    fn port_name(&self) -> Option<String> {
-        self.inner.port_name()
+    fn name(&self) -> Option<String> {
+        self.inner.name()
     }
 
     /// Returns the current baud rate.
@@ -142,7 +142,7 @@ impl SerialPort for Serial {
     /// This function returns `None` if the baud rate could not be determined. This may occur if
     /// the hardware is in an uninitialized state. Setting a baud rate with `set_baud_rate()`
     /// should initialize the baud rate to a supported value.
-    fn baud_rate(&self) -> Option<::BaudRate> {
+    fn baud_rate(&self) -> Result<u32> {
         self.inner.baud_rate()
     }
 
@@ -152,7 +152,7 @@ impl SerialPort for Serial {
     /// if the hardware is in an uninitialized state or is using a non-standard character size.
     /// Setting a baud rate with `set_char_size()` should initialize the character size to a
     /// supported value.
-    fn data_bits(&self) -> Option<::DataBits> {
+    fn data_bits(&self) -> Result<::DataBits> {
         self.inner.data_bits()
     }
 
@@ -162,7 +162,7 @@ impl SerialPort for Serial {
     /// occur if the hardware is in an uninitialized state or is using an unsupported flow control
     /// mode. Setting a flow control mode with `set_flow_control()` should initialize the flow
     /// control mode to a supported value.
-    fn flow_control(&self) -> Option<::FlowControl> {
+    fn flow_control(&self) -> Result<::FlowControl> {
         self.inner.flow_control()
     }
 
@@ -171,7 +171,7 @@ impl SerialPort for Serial {
     /// This function returns `None` if the parity mode could not be determined. This may occur if
     /// the hardware is in an uninitialized state or is using a non-standard parity mode. Setting
     /// a parity mode with `set_parity()` should initialize the parity mode to a supported value.
-    fn parity(&self) -> Option<::Parity> {
+    fn parity(&self) -> Result<::Parity> {
         self.inner.parity()
     }
 
@@ -181,13 +181,13 @@ impl SerialPort for Serial {
     /// occur if the hardware is in an uninitialized state or is using an unsupported stop bit
     /// configuration. Setting the number of stop bits with `set_stop-bits()` should initialize the
     /// stop bits to a supported value.
-    fn stop_bits(&self) -> Option<::StopBits> {
+    fn stop_bits(&self) -> Result<::StopBits> {
         self.inner.stop_bits()
     }
 
     /// Returns the current timeout.
     fn timeout(&self) -> Duration {
-        Duration::from_secs(0)
+        self.settings().timeout
     }
 
     // Port settings setters
@@ -206,7 +206,7 @@ impl SerialPort for Serial {
     /// If the implementation does not support the requested baud rate, this function may return an
     /// `InvalidInput` error. Even if the baud rate is accepted by `set_baud_rate()`, it may not be
     /// supported by the underlying hardware.
-    fn set_baud_rate(&mut self, baud_rate: ::BaudRate) -> serialport::Result<()> {
+    fn set_baud_rate(&mut self, baud_rate: u32) -> serialport::Result<()> {
         self.inner.set_baud_rate(baud_rate)
     }
 
@@ -418,7 +418,8 @@ impl<'a> Write for &'a Serial {
 
     fn flush(&mut self) -> io::Result<()> {
         termios::tcdrain(self.inner.as_raw_fd()).map_err(|e| {
-            let e: ::Error = e.into();
+            let e: nix::Error = e;
+            //FIXME How to change nix::Error to io::Error?
             e.into()
         })
     }
